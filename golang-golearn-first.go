5a4b1275d0272e98daa70b580f01cbb8252eabda
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-10 10:53:32 +0900

    Imports add

--- golang-golearn-first.go
+++ golang-golearn-first.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"math"
 )
 
 func main() {
 
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println("My favorite number is", rand.Intn(100))
+	// 3rd
+	//Sqrt returns the square root of x.
+	fmt.Printf("Now you have %e problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %E problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %f problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %F problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %G problems.\n", math.Sqrt(7))
 
-	fmt.Println("Hello, World!")
+	// 2nd
+	/*
+		rand.Seed(time.Now().UnixNano())
+		fmt.Println("My favorite number is", rand.Intn(100))
+	*/
+
+	// 1st
+	/*	fmt.Println("Hello, World!")
+	 */
 }
