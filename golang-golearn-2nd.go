43d287bafbcd3b3482a15d18815063992c9dcdaa
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-11 08:39:03 +0900

    if-with-a-short-statement add

--- golang-golearn-2nd.go
+++ golang-golearn-2nd.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+//No.4
+/*
 func sqrt(x float64) string {
 	if x < 0 {
 		// return sqrt(x) <- but code
@@ -12,10 +14,26 @@ func sqrt(x float64) string {
 	}
 	return fmt.Sprint(math.Sqrt(x))
 }
+*/
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
 
 func main() {
+	//No.6
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
 	//No.5
-	fmt.Println(sqrt(2), sqrt(-2))
+	/*
+		fmt.Println(sqrt(2), sqrt(-2))
+	*/
 
 	//No.4
 	/*
