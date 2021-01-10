8a1dbc62fc2e848e520032ff2ac872592f07d90d
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-11 08:57:25 +0900

    If and else add

--- golang-golearn-2nd.go
+++ golang-golearn-2nd.go
@@ -16,19 +16,41 @@ func sqrt(x float64) string {
 }
 */
 
+//No.6
+/*
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
 	}
 	return lim
 }
+*/
+
+//No.7
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
 
 func main() {
-	//No.6
+	//No.7
 	fmt.Println(
 		pow(3, 2, 10),
 		pow(3, 3, 20),
 	)
+
+	//No.6
+	/*
+		fmt.Println(
+			pow(3, 2, 10),
+			pow(3, 3, 20),
+		)
+	*/
 
 	//No.5
 	/*
