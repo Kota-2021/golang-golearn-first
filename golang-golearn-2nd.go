8d3a3bafab6f179339edb0df5b1a72289c8d9a19
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-11 08:32:10 +0900

    If add

--- golang-golearn-2nd.go
+++ golang-golearn-2nd.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func main() {
-	//No.4
-	for {
+func sqrt(x float64) string {
+	if x < 0 {
+		// return sqrt(x) <- but code
+		return sqrt(-x) + "i"
 	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	//No.5
+	fmt.Println(sqrt(2), sqrt(-2))
+
+	//No.4
+	/*
+		for {
+		}
+	*/
 
 	//No.2 & No.3
-	sum := 1
-	for sum < 1000 {
-		sum += sum
-	}
-	fmt.Println(sum)
+	/*
+		sum := 1
+		for sum < 1000 {
+			sum += sum
+		}
+		fmt.Println(sum)
+	*/
 
 	//No.1
 	/*
