2e8d34b22ba5d15c397aa0cc3dc816f543bc883a
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-14 21:07:39 +0900

    Structs add

--- golang-golearn-3rd.go
+++ golang-golearn-3rd.go
@@ -1,19 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
 
 func main() {
+	//No.2
+	fmt.Println(Vertex{1, 2})
+	m := Vertex{5, 10}
+	fmt.Println(m.X)
+	fmt.Println(m.Y)
+
 	//No.1
-	i, j := 42, 2701
+	/* 	i, j := 42, 2701
 
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
+	   	p := &i         // point to i
+	   	fmt.Println(*p) // read i through the pointer
+	   	*p = 21         // set i through the pointer
+	   	fmt.Println(i)  // see the new value of i
 
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+	   	p = &j         // point to j
+	   	*p = *p / 37   // divide j through the pointer
+	   	fmt.Println(j) // see the new value of j */
 }
