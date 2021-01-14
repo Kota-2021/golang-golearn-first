a31b3687cfc204cf6016aa17d893d58117a37d02
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-14 21:38:30 +0900

    Struct Literals add

--- golang-golearn-3rd.go
+++ golang-golearn-3rd.go
@@ -15,17 +15,32 @@ import "fmt"
 } */
 
 //No.4
-type Vertex struct {
+/* type Vertex struct {
 	X int
 	Y int
+} */
+
+//No.5
+type Vertex struct {
+	X, Y int
 }
 
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
 func main() {
+	//No.5
+	fmt.Println(v1, p, v2, v3)
+
 	//No.4
-	v := Vertex{1, 2}
+	/* v := Vertex{1, 2}
 	p := &v
 	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v) */
 
 	//No.3
 	/* v := Vertex{1, 2}
