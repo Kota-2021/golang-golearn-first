c31bedf919ab9f0ccc21dbe1fe539cb2e26fe9f3
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-16 22:06:38 +0900

    Nil slices

--- golang-golearn-3rd.go
+++ golang-golearn-3rd.go
@@ -33,26 +33,33 @@ var (
 ) */
 
 //No.11
-func printSlice(s []int) {
+/* func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
-}
+} */
 
 func main() {
+	//No.12
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
+
 	//No.11
-	s := []int{2, 3, 5, 7, 11, 13}
-	printSlice(s)
+	/* 	s := []int{2, 3, 5, 7, 11, 13}
+	   	printSlice(s)
 
-	// Slice the slice to give it zero length.
-	s = s[:0]
-	printSlice(s)
+	   	// Slice the slice to give it zero length.
+	   	s = s[:0]
+	   	printSlice(s)
 
-	// Extend its length.
-	s = s[:4]
-	printSlice(s)
+	   	// Extend its length.
+	   	s = s[:4]
+	   	printSlice(s)
 
-	// Drop its first two values.
-	s = s[2:]
-	printSlice(s)
+	   	// Drop its first two values.
+	   	s = s[2:]
+	   	printSlice(s) */
 
 	//No.10
 	/* 	s := []int{2, 3, 5, 7, 11, 13}
